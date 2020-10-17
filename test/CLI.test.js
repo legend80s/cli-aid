35192f5ef2145dd6337f61ad60aec4d2e0745d71
chuanzong.lcz@alibaba-inc.com <chuanzong.lcz@alibaba-inc.com>
2020-10-18 01:51:50 +0800

    test: pass name version implicitly

--- test/CLI.test.js
+++ test/CLI.test.js
@@ -15,11 +15,7 @@ describe('CLI', () => {
   });
 
   test('should parse as expected when fill schema with option method', () => {
-    const actual = new CLI([
-        'https://gw.alipayobjects.com/mdn/member_frontWeb/afts/img/A*h7o9Q4g2KiUAAAAAAAAAAABkARQnAQ',
-        '--output=./alipay-logo-2.png',
-        '--debug',
-      ])
+    const actual = new CLI({ name: 'tinify-client', version: '2.0.0' })
       .option('key', { to: CLI.toString, help: 'The Tinify key. Accessible at https://tinypng.com/developers.' })
       .option('src', { to: CLI.toString, help: 'Image url or local image path to compress.' })
       .option('output', 'o', { to: CLI.toString, help: 'The compressed image file path.' })
@@ -29,7 +25,11 @@ describe('CLI', () => {
 
       .option('debug', { to: CLI.toBoolean, help: 'Show the parsed CLI params.' })
 
-      .parse();
+      .parse([
+        'https://gw.alipayobjects.com/mdn/member_frontWeb/afts/img/A*h7o9Q4g2KiUAAAAAAAAAAABkARQnAQ',
+        '--output=./alipay-logo-2.png',
+        '--debug',
+      ]);
 
     const expected = new Map([
       ['key', undefined],
@@ -61,12 +61,12 @@ describe('CLI', () => {
       ['debug', { to: CLI.toBoolean, help: 'Show the parsed CLI params.' }],
     ];
 
-    const actual = new CLI([
+    const actual = new CLI({ name: 'cli-aid', version: '2.0.0', schema })
+      .parse([
         'https://gw.alipayobjects.com/mdn/member_frontWeb/afts/img/A*h7o9Q4g2KiUAAAAAAAAAAABkARQnAQ',
         '--output=./alipay-logo-2.png',
         '--debug',
-      ], schema)
-      .parse();
+      ]);
 
     const expected = new Map([
       ['key', undefined],
@@ -84,5 +84,19 @@ describe('CLI', () => {
     ]);
 
     expect(actual).toEqual(expected)
+  });
+
+  test('should show version tips as expected', () => {
+    const cli = new CLI({ name: 'cli-aid', version: '2.0.0' })
+    const actual = cli.parse();
+
+    const expected = new Map([
+      ['help', false],
+      ['version', false],
+      ['rest', []],
+    ]);
+
+    expect(actual).toEqual(expected)
+    expect(cli.versionTips).toEqual(`cli-aid/2.0.0 ${process.platform}-${process.arch} node-${process.version}`)
   });
 });
