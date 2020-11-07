4f64de94f0f3b21651c86c058f8f693d2f566843
chuanzong.lcz@alibaba-inc.com <chuanzong.lcz@alibaba-inc.com>
2020-11-07 15:01:50 +0800

    feat: demo tips from pkg

--- demo/tinify-client-cli.js
+++ demo/tinify-client-cli.js
@@ -1,21 +1,22 @@
 const { CLI } = require('../src');
-
-// console.log('process.argv.slice(2):', process.argv.slice(2));
-
 const pkg = {
   name: 'tinify-client',
-  version: '3',
+  version: '4.3.0',
   description: 'A CLI to compress your images not only intelligently but also to the EXTREME!',
 };
 
-new CLI({ name: 'tinify-client-cli', version: '2.0.0' })
+// console.log('process.argv.slice(2):', process.argv.slice(2));
+
+new CLI()
   .package(pkg)
-  .option('max-count', 'm', { default: 15, help: 'The max compressing turns. Default 15.' })
-  .option('in-place', 'i', { default: false, help: 'Overwrite the original image. Default false.' })
+  .option('dry-run', { default: false, help: 'Does everything compress would do except actually compressing. Reports the details of what would have been compressed' })
+  .option('max-count', 'm', 'c', { default: 15, help: 'The max compressing turns. Default 15.' })
   .command('base64', {
-    usage: 'tinify-client-cli base64 IMG_URL_OR_LOCAL_IMG_PATH',
-    help: 'output base64-encoded string of the input image',
+    usage: 'npx tinify-client base64 IMG_URL_OR_LOCAL_IMG_PATH',
+    help: 'Output base64-encoded string of the input image.',
   }, (options) => {
     console.log('output base64 with options:', options);
+
+    process.exit(0);
   })
   .parse(process.argv.slice(2));
