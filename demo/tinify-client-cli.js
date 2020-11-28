7f562fc4e04d2de43bf548d2d9500aedc5abe3a7
chuanzong.lcz@alibaba-inc.com <chuanzong.lcz@alibaba-inc.com>
2020-11-28 17:49:59 +0800

    docs: update demo to add a command

--- demo/tinify-client-cli.js
+++ demo/tinify-client-cli.js
@@ -10,15 +10,19 @@ const pkg = {
 
 new CLI()
   .package(pkg)
-  // .usage('npx tinify-client IMG_URL_OR_LOCAL_IMG_PATH [OPTIONS]')
+  // .usage('tinify IMG_URL_OR_LOCAL_IMG_PATH [OPTIONS]')
   .option('dry-run', { default: false, help: 'Does everything compress would do except actually compressing. Reports the details of what would have been compressed.' })
   .option('max-count', 'm', 'c', { default: 15, help: 'The max compressing turns. Default 15.' })
   .command('base64', {
-    usage: 'npx tinify-client base64 IMG_URL_OR_LOCAL_IMG_PATH',
+    usage: 'tinify base64 <IMG_URL_OR_LOCAL_IMG_PATH>',
     help: 'Output base64-encoded string of the input image.',
   }, (options) => {
     console.log('output base64 with options:', options);
 
     process.exit(0);
+  })
+  .command('set-key', {
+    usage: 'tinify set-key <KEY>',
+    help: 'Set the tinify key.',
   })
   .parse(process.argv.slice(2));
